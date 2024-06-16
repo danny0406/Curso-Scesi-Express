fcd671f22ed3439450b5102f392022cd65d1e48b
danny0406 <kevinpoma312@gmail.com>
2024-06-16 14:19:23 -0400

    ADD users.models

---
+++ src/models/users.models.js
@@ -0,0 +1,42 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database.config');
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    timestamps: true,
+  }
+);
+
+module.exports = User;
